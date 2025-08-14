35c7b5d4da16e0704462f85b68b972f7c2ffbc0e
Fovir <fovir@disroot.org>
2025-08-14 16:22:26 +0800

    refactor(todo-event): Move the checkbox to a new component.

--- src/components/TodoEvent/TodoEvent.jsx
+++ src/components/TodoEvent/TodoEvent.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TodoEvent.css";
+import MyCheckBox from "../MyCheckBox/MyCheckBox";
 
 export default function TodoEvent({ eventName, done = false }) {
   return (
     <div className="todo-event">
-      <input type="checkbox" checked={done} />
+      <MyCheckBox boxStatus={done} />
       <label>{eventName}</label>
     </div>
   );
