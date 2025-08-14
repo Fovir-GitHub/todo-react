c4dd4bf5183c32b7ba4e32959767248e7329b419
Fovir <fovir@disroot.org>
2025-08-14 16:55:39 +0800

    feat(MyCheckBox): Add styles for my check box.

--- src/components/MyCheckBox/MyCheckBox.jsx
+++ src/components/MyCheckBox/MyCheckBox.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
+import "./MyCheckBox.css";
 
-export default function MyCheckBox({ boxStatus = false }) {
+export default function MyCheckBox({ eventName, boxStatus = false }) {
   const [status, setStatus] = useState(boxStatus);
 
   return (
-    <>
-      <input
-        type="checkbox"
-        checked={status}
-        onChange={() => {
-          setStatus(!status);
-        }}
-      />
-    </>
+    <label
+      className="my-checkbox"
+      onChange={() => {
+        setStatus(!status);
+      }}
+    >
+      <span className={`event-name ${status ? "checked" : ""}`}>
+        {eventName}
+      </span>
+      <input type="checkbox" checked={status} />
+      <span className="checkmark"></span>
+    </label>
   );
 }
